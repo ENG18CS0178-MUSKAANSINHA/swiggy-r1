d33270cabb4befb7e22ccc42ce4ba6993e4f6541
Muskaan Sinha <muskaan.sinha2708@gmail.com>
2024-06-22 16:24:53 +0530

    Arena Game

--- src/player.js
+++ src/player.js
@@ -8,6 +8,10 @@ class Player {
     isAlive() {
         return this.health > 0;
     }
+
+    toString() {
+        return `Player(Health: ${this.health}, Strength: ${this.strength}, Attack: ${this.attack})`;
+    }
 }
 
 module.exports = Player;
